7d590837a5418460e2b5a0dc039648448f874285
kaikyou76 <kaikyou@gmail.com>
2023-07-10 08:24:22 +0900

    ok

--- 複雑なupdate文.sql
+++ 複雑なupdate文.sql
@@ -18,6 +18,25 @@ WHERE trn_phone.phone_id IN (
 SELECT
     A.user_id,
     E.company_cd,
+    IPAD(E.department_cd, 5, '0'),
+    E.department_nm,
+    O.print_order
+FROM
+    tmp_integratedid_employee E
+    INNER JOIN mst_section M ON E.department_cd = M.section_cd
+    INNER JOIN trn_user A ON E.employee_cd = A.biz_employee_id
+	INNER JOIN rel_user_section R ON A.user_id = R.user_id
+    INNER JOIN tmp_integratedid_organization O ON E.organization_cd = O.Organization_cd
+WHERE
+    E.company_cd = #{companyCode}
+    AND LPAD(E.department_cd, 5, '0') = #{departmentCode}
+    AND E.employee_cd = #{employeeCode}
+</select>
+
+<select id="selAdditionUserSectionDetail" resultType="Map">
+SELECT
+    A.user_id,
+    E.company_cd,
     LPAD(E.department_cd, 5, '0') AS department_cd,
     E.department_nm,
     O.print_order
@@ -25,10 +44,12 @@ FROM
     tmp_integratedid_employee E
     INNER JOIN mst_section M ON E.department_cd = M.section_cd
     INNER JOIN trn_user A ON E.employee_cd = A.biz_employee_id
+    INNER JOIN rel_user_section R ON A.user_id = R.user_id
     INNER JOIN tmp_integratedid_organization O ON E.organization_cd = O.Organization_cd
 WHERE
     E.company_cd = #{companyCode}
     AND LPAD(E.department_cd, 5, '0') = #{departmentCode}
     AND E.employee_cd = #{employeeCode}
 </select>
+
 
