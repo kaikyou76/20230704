a01bd15da4b051830e0ed219d7ee7c63f2966f86
kaikyou76 <kaikyou@gmail.com>
2023-07-27 19:50:28 +0900

    ok

--- CUCMメンテナンスシステム開発/タスクメモ帳/03一時取込み処理/version3.0/BatRuntimeException.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.batch.core.step.skip.SkipRuntimeException;
-
-public class BatRuntimeException extends SkipRuntimeException {
-
-    public BatRuntimeException(String message) {
-        super(message);
-    }
-}
